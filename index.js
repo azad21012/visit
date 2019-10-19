fcf8c59097f810f8f20aebd460351e96049391f7
azad21012 <azad.zarshad@yahoo.com>
2019-10-19 19:30:37 +0330

    add files

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const redis = require("redis");
+
+const app =express();
+const client = redis.createClient();
+ 
+
+client.set('visits',0)
+// if you'd like to select database 3, instead of 0 (default), call
+// client.select(3, function() { /* ... */ });
+ 
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+app.get('/',(req,res)=>{
+    client.get('visits',(err,result)=>{
+        res.send('you visits this site:'+result);
+        client.set('visits',parseInt(result)+1)
+    })
+})
+
+app.listen(8081,()=>console.log('listen to 8081'));
